6fd6137cde25ddfc6c3d8f195e6ce330bdd0db21
Hugo Delgado <hugo.cdelgado925@gmail.com>
2022-07-18 17:20:49 -0700

    inital commit

---
+++ YonkoBanking/BankAccount.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YonkoBanking
+{
+    public class BankAccount
+    {
+        //static means its shared by all objects
+        private static int accountNumberSeed = 1234567890;
+        public string Number { get; }
+        public string Owner { get; set; }
+      
+        //Balance gets calculated everytime instead of being directly modified
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+        }
+        //constructor to initialze objs
+        private readonly decimal _minimumBalance;
+
+        public BankAccount(string name, decimal initialBalance) : this(name, initialBalance, 0) { }
+
+        public BankAccount(string name, decimal initialBalance, decimal minimumBalance)
+        {
+            Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
+            Owner = name;
+            _minimumBalance = minimumBalance;
+            if (initialBalance > 0)
+                MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+        }
+        private List<Transaction> allTransactions = new List<Transaction>();
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+        public string GetAccountHistory()
+        {
+            var report = new System.Text.StringBuilder();
+
+            decimal balance = 0;
+            report.AppendLine("Date\t\tAmount\tBalance\tNote");
+            foreach (var item in allTransactions)
+            {
+                balance += item.Amount;
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Notes}");
+            }
+
+            return report.ToString();
+        }
+        //polymorphism
+        //virtual keyword lets classes override it
+        public virtual void PerformMonthEndTransactions() { }
+
+    }
+}
---
+++ YonkoBanking/model/AccountTypes.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YonkoBanking.model
+{
+    
+    public class InterestEarningAccount : BankAccount
+    {
+      
+
+        public InterestEarningAccount(string name, decimal initialBalance) : base(name, initialBalance)
+        {
+        }
+        public override void PerformMonthEndTransactions()
+        {
+            if (Balance > 500m)
+            {
+                decimal interest = Balance * 0.05m;
+                MakeDeposit(interest, DateTime.Now, "apply monthly interest");
+            }
+        }
+    }
+
+    public class LineOfCreditAccount : BankAccount
+    {
+        public LineOfCreditAccount(string name, decimal initialBalance) : base(name, initialBalance)
+        {
+        }
+
+        public override void PerformMonthEndTransactions()
+        {
+            if (Balance < 0)
+            {
+                // Negate the balance to get a positive interest charge:
+                decimal interest = -Balance * 0.07m;
+                MakeWithdrawal(interest, DateTime.Now, "Charge monthly interest");
+            }
+        }
+    }
+
+    public class GiftCardAccount : BankAccount
+    {
+        private readonly decimal _monthlyDeposit = 0m;
+
+        public GiftCardAccount(string name, decimal initialBalance, decimal monthlyDeposit = 0) : base(name, initialBalance)
+            => _monthlyDeposit = monthlyDeposit;
+        public override void PerformMonthEndTransactions()
+        {
+            if (_monthlyDeposit != 0)
+            {
+                MakeDeposit(_monthlyDeposit, DateTime.Now, "Add monthly deposit");
+            }
+        }
+    }
+}
